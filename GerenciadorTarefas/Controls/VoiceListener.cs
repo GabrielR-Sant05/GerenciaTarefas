b5a29ab379adee48f9dabcef74cba811ff940118
GabrielR-Sant05 <gabrielsantos88974@outlook.com>
2025-05-13 15:52:54 -0300

    vosk

---
+++ GerenciadorTarefas/Controls/VoiceListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vosk;
+
+namespace GerenciadorTarefas.Controls
+{
+    class VoiceListener
+    {
+        //string rootPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\.."));
+        void TesteLib()
+        {
+            var model = new Model("Data/model"); // Substitua pelo caminho correto
+            System.Console.WriteLine("Modelo carregado com sucesso!");
+        }
+    }
+}
